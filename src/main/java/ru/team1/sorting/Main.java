30c87f8afcd0fb5868aef7c6ae00fafc51d5c80c
xvzrlx <120486395+xvzrlx@users.noreply.github.com>
2025-09-12 18:15:06 +0400

    Merge pull request #1 from xvzrlx/DK-Ivanov
    Init commit)

--- src/main/java/ru/team1/sorting/Main.java
+++ src/main/java/ru/team1/sorting/Main.java
@@ -1,4 +1,8 @@
 package ru.team1.sorting;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("DK-Ivanov");
+        System.out.println("part 2");
+    }
 }
